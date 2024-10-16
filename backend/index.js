d4d84cf5b8b58c597a4d184f2264e80fa776bf71
Imre <rabl.imre95@gmail.com>
2024-10-16 17:52:12 +0200

    added frontend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
@@ -12,30 +12,27 @@ app.get('/', (req, res) => {
 
 app.use('/public', express.static(path.join(`${__dirname}/../frontend/static`)));
 
+app.get('/api/users', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/data.json`));
+});
 
-app.get('/api/users',(req, res) => {
-    res.sendFile(path.join(`${__dirname}/data.json`));
-  });
+app.post('/api/users', (req, res) => {
+  console.log(req.body);
 
-  app.post('/api/users',(req, res) => { 
-    console.log(req.body);
+  fs.readFile(`${__dirname}/data.json`, (err, rawData) => {
+    const currentData = JSON.parse(rawData);
+    const newData = {
+      id: currentData[currentData.length - 1].id + 1,
+      ...req.body
+    };
 
-    fs.readFile(`${__dirname}/data.json`, (err, rawData) => {
-        const currentData = JSON.parse(rawData)
-        const newData = {
-            id: currentData[currentData.length - 1].id +1,
-            ...req.body
-        }
+    currentData.push(newData);
 
-        currentData.push(newData)
-
-        fs.writeFile(`${__dirname}/data.json`, JSON.stringify(currentData, null, 2), () => {
-            res.json(`new data has been added with id ${newData.id}`)
-        })
+    fs.writeFile(`${__dirname}/data.json`, JSON.stringify(currentData, null, 2), () => {
+      res.json(`new data has been added with id ${newData.id}`);
     })
-
-    res.json("ok")
   })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
